c4a08ab483abbe980bde45fa2e603c907ae0ff18
Mark Risher <mark_risher@yahoo.com>
2017-07-01 09:57:13 -0700

    replacing images

--- ARKitTutorial/ViewController.swift
+++ ARKitTutorial/ViewController.swift
@@ -54,14 +54,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
+    
     // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+        
+        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.05)
+        box.firstMaterial!.diffuse.contents = UIColor.blue
+        let cubeNode = SCNNode(geometry: box)
+            
+        cubeNode.position = SCNVector3(0, 0, -0.2) // SceneKit/AR coordinates are in meters
+        
+        sceneView.scene.rootNode.addChildNode(cubeNode)
+        
         return node
     }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
